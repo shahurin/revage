16d86550876a9f9f4286c13e83ecce116e6fc781
R101-124\lightbox <2312097@i-seifu.jp>
2025-09-04 16:39:22 +0900

    navigation.phpのついか

---
+++ chat/navigation.php
@@ -0,0 +1,27 @@
+<!-- 下部ナビゲーション -->
+<nav class="bottom-nav">
+        <a href="revege.php" class="nav-item active">
+            <span class="material-icons-outlined">home</span>
+            <span>ホーム</span>
+        </a>
+        <a href="#" class="nav-item"> <!-- search.phpなどに変更 -->
+            <span class="material-icons-outlined">search</span>
+            <span>検索</span>
+        </a>
+        <a href="sell.php" class="nav-item">
+            <span class="material-icons-outlined">add_circle_outline</span>
+            <span>出品</span>
+        </a>
+        <a href="pf.php" class="nav-item">
+            <span class="material-icons-outlined">person_outline</span>
+            <span>カート</span>
+        </a>
+        <a href="chat.php" class="nav-item">
+            <span class="material-icons-outlined"></span>
+            <span>メッセージ</span>
+        </a>
+        <a href="pf.php" class="nav-item">
+            <span class="material-icons-outlined">person_outline</span>
+            <span>プロフィール</span>
+        </a>
+    </nav>
