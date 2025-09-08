3e84f17d555233aab8aa19b0a23f52286e616efd
R101-124\lightbox <2312097@i-seifu.jp>
2025-09-08 13:57:10 +0900

    pfにログアウト機能追加

--- chat/pf.php
+++ chat/pf.php
@@ -77,6 +77,15 @@
                             <svg class="arrow-icon" viewBox="0 0 24 24"><path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"></path></svg>
                         </a>
                     </li>
+                    <li>
+                        <a href="logout.php" class="settings-item">
+                            <div class="setting-content">
+                                <svg class="setting-icon" viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 17h-2v-2h2v2zm2.07-7.75l-.9.92C13.45 12.9 13 13.5 13 15h-2v-.5c0-1.1.45-2.1 1.17-2.83l1.24-1.26c.37-.36.59-.86.59-1.41 0-1.1-.9-2-2-2s-2 .9-2 2H8c0-2.21 1.79-4 4-4s4 1.79 4 4c0 .88-.36 1.68-.93 2.25z"></path></svg>
+                                <span>ログアウト</span>
+                            </div>
+                            <svg class="arrow-icon" viewBox="0 0 24 24"><path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"></path></svg>
+                        </a>
+                    </li>
                 </ul>
             </section>
         </main>
